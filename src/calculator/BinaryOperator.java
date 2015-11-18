dcedb1ce9c6fba52505a71854ce0e5b1ff38d9e7
Andrés Luna <andresluna2007@gmail.com>
2015-11-18 11:50:28 -0300

    package renamed.

---
+++ src/calculator/BinaryOperator.java
@@ -0,0 +1,5 @@
+package calculator;
+
+public enum BinaryOperator {
+	ADDITION,SUBSTRACTION,MULTIPLICATION,DIVISION;
+}
---
+++ src/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class Calculator {
+	Calculator() {
+		
+	}
+	
+	public float evaluateExpression(Expression e) {
+		return e.evaluate();
+		
+	}
+}
